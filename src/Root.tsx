61517c472622f8d8cbefb589b329fdd3f107a0d8
dhythm <yuta.okada.20@gmail.com>
2026-03-16 09:12:15 +0900

    Refactor RemotionRoot component to use MainVideo composition with updated dimensions and duration

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,18 +1,16 @@
-import "./index.css";
+import React from "react";
 import { Composition } from "remotion";
-import { MyComposition } from "./Composition";
+import { MainVideo } from "./MainVideo";
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <>
-      <Composition
-        id="MyComp"
-        component={MyComposition}
-        durationInFrames={60}
-        fps={30}
-        width={1280}
-        height={720}
-      />
-    </>
+    <Composition
+      id="MainVideo"
+      component={MainVideo}
+      durationInFrames={2536}
+      fps={30}
+      width={1760}
+      height={1596}
+    />
   );
 };
